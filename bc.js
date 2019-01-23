963523b9029483cd7c359b57741bcf7f08e3787b
L4amiNo <42951746+L4amiNo@users.noreply.github.com>
2019-01-23 06:44:10 -0800

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,14 @@
+
+client.login('mfa.TTwlWs5mL0O3oN8NPfcrBDiSduxhLrif0GeWBDfCX1bh_vedk-szXvCxGgRYwGGXTvwjWaGgGXeySw9h5ole');
+
+
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("535461700225204235");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**:Yoo: WelcomE  `×` To `×` Outside , ! :AmimatedHeart: **`), 4000)        
+}
+});
